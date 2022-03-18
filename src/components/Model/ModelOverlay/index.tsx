c853df3d11540101b3811df82360de786a67bcd6
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-18 15:41:31 -0300

    :rocket: importando children no ModelOverlay

--- src/components/Model/ModelOverlay/index.tsx
+++ src/components/Model/ModelOverlay/index.tsx
@@ -2,12 +2,8 @@ import React from "react";
 
 import { Container } from "./styles";
 
-const ModelOverlay: React.FC = () => {
-  return (
-    <Container>
-      <h1>ModelOverlay</h1>
-    </Container>
-  );
+const ModelOverlay: React.FC = ({ children }) => {
+  return <Container>{children}</Container>;
 };
 
 export default ModelOverlay;
