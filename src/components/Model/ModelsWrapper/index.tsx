514a52c3353566369a1c450822ead2023e24261f
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-18 15:02:16 -0300

    :pencil: refatorando e adicionando contextProvider no projeto

--- src/components/Model/ModelsWrapper/index.tsx
+++ src/components/Model/ModelsWrapper/index.tsx
@@ -1,5 +1,5 @@
-import React, { useRef, useState } from "react";
-import { CarModel } from "../ModelsContext";
+import React, { useCallback, useRef, useState } from "react";
+import ModelsContext, { CarModel } from "../ModelsContext";
 
 import { Container } from "./styles";
 
@@ -8,7 +8,38 @@ const ModelsWrapper: React.FC = ({ children }) => {
 
   const [registeredModels, setRegisteredModels] = useState<CarModel[]>([]);
 
-  return <Container ref={wrapperRef}>{children}</Container>;
+  const registerModel = useCallback((model: CarModel) => {
+    setRegisteredModels((state) => [...state, model]);
+  }, []);
+
+  const unregisterModel = useCallback((modelName: string) => {
+    setRegisteredModels((state) =>
+      state.filter((model) => model.modelName !== modelName)
+    );
+  }, []);
+
+  const getModelByName = useCallback(
+    (modelName: string) => {
+      return (
+        registeredModels.find((item) => item.modelName === modelName) || null
+      );
+    },
+    [registeredModels]
+  );
+
+  return (
+    <ModelsContext.Provider
+      value={{
+        wrapperRef,
+        registeredModels,
+        registerModel,
+        unregisterModel,
+        getModelByName,
+      }}
+    >
+      <Container ref={wrapperRef}>{children}</Container>;
+    </ModelsContext.Provider>
+  );
 };
 
 export default ModelsWrapper;
