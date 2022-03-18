df1bd8eb34a7b8f179a62229e788491ad7f7f072
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-18 11:10:43 -0300

    :twisted_rightwards_arrows: adicionando components no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { GlobalStyles } from "./styles/GlobalStyles";
+import Page from "./components/Page";
 
 function App() {
-  return <h1>Hellow</h1>;
+  return;
+  // eslint-disable-next-line no-unreachable
+  <>
+    <Page />
+
+    <GlobalStyles />
+  </>;
 }
 
 export default App;
