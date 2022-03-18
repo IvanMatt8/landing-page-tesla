64f4e9d464d36e86a1651bb0ccee62f5303f6429
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-18 15:30:47 -0300

    :pencil: import do ModelOverlay

--- src/components/Model/ModelsWrapper/index.tsx
+++ src/components/Model/ModelsWrapper/index.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useRef, useState } from "react";
 import ModelsContext, { CarModel } from "../ModelsContext";
-
-import { Container } from "./styles";
+import ModelOverlay from "../ModelOverlay";
+import { Container, OverlaysRoot } from "./styles";
 
 const ModelsWrapper: React.FC = ({ children }) => {
   const wrapperRef = useRef<HTMLDivElement>(null);
@@ -37,7 +37,16 @@ const ModelsWrapper: React.FC = ({ children }) => {
         getModelByName,
       }}
     >
-      <Container ref={wrapperRef}>{children}</Container>;
+      <Container ref={wrapperRef}>
+        <OverlaysRoot>
+          {registeredModels.map((item) => (
+            <ModelOverlay key={item.modelName}>{item.overlayNode}</ModelOverlay>
+          ))}
+        </OverlaysRoot>
+
+        {children}
+      </Container>
+      ;
     </ModelsContext.Provider>
   );
 };
