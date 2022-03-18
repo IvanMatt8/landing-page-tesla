22aa43be8077f100bac36c44bddba93088f0ebf8
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-18 14:30:58 -0300

    :rocket: import do hook useState

--- src/components/Model/ModelsWrapper/index.tsx
+++ src/components/Model/ModelsWrapper/index.tsx
@@ -1,9 +1,12 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import { CarModel } from "../ModelsContext";
 
 import { Container } from "./styles";
 
 const ModelsWrapper: React.FC = ({ children }) => {
   const wrapperRef = useRef<HTMLDivElement>(null);
+
+  const [registeredModels, setRegisteredModels] = useState<CarModel[]>([]);
 
   return <Container ref={wrapperRef}>{children}</Container>;
 };
